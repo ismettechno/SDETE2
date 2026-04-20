6ee8714a4b7e1edcee005e37f9615e982cc21f6c
TechnoStudy <ismettechno@gmail.com>
2026-04-20 19:31:30 +0100

    Initial commit

---
+++ src/Gun02/_01_OpeningWebsite.java
@@ -0,0 +1,22 @@
+package Gun02;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class _01_OpeningWebsite {
+    public static void main(String[] args) {
+
+        WebDriver tarayici=new ChromeDriver();
+
+        tarayici.get("https://www.google.com/");
+
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        tarayici.quit();
+    }
+}
