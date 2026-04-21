22a3065d1859fa77370f414bfc9e406d1a7b3b52
TechnoStudy <ismettechno@gmail.com>
2026-04-21 19:28:34 +0100

    _06_FindingByLinkText

---
+++ src/Gun03/_06_FindingByLinkText.java
@@ -0,0 +1,24 @@
+package Gun03;
+
+import Utility.MyFunc;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class _06_FindingByLinkText {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.hepsiburada.com/");
+
+        // SADECE a TAG lerinde çalışır  linkText
+        WebElement siparislerimLinki=driver.findElement(By.linkText("Siparişlerim"));
+        System.out.println("siparislerimLinki.getText() = " + siparislerimLinki.getText());
+
+        WebElement link2=driver.findElement(By.partialLinkText("Süper Fiyat"));
+        System.out.println("link2.getText() = " + link2.getText());
+
+        MyFunc.Bekle(3);
+        driver.quit();
+    }
+}
