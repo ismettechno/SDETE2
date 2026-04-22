f030d95ed52b4eee170cdbe18dd3f22632646088
TechnoStudy <ismettechno@gmail.com>
2026-04-22 18:28:15 +0100

    _02_Soru soru

---
+++ src/Gun04/_02_Soru.java
@@ -0,0 +1,10 @@
+package Gun04;
+
+public class _02_Soru {
+    // Senaryo:
+    // 1-  https://form.jotform.com/221934510376353  sayfasınız açınız
+    // 2-  isim kutucuğuna adınızı yazdırınız
+    // 3-  soyad kutucuğuna soyadınız yazdırınız
+    // 4-  Submit butona tıklatınız (click)
+    // 5-  "Thank You" yazısını görünüyor ise "TEST PASSED" yazdırınız
+}
