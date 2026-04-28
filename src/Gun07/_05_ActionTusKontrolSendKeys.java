385c81c6511bfef3c145aeeb44b3ff00448de2e6
TechnoStudy <ismettechno@gmail.com>
2026-04-28 19:31:54 +0100

    _05_ActionTusKontrolSendKeys

---
+++ src/Gun07/_05_ActionTusKontrolSendKeys.java
@@ -0,0 +1,32 @@
+package Gun07;
+
+import Utility.BaseDriver;
+import Utility.MyFunc;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+public class _05_ActionTusKontrolSendKeys extends BaseDriver  {
+
+    @Test
+    public void test1() {
+        driver.get("https://demoqa.com/auto-complete");
+        MyFunc.Bekle(3);
+
+        WebElement txtBox = driver.findElement(By.id("autoCompleteMultipleInput"));
+
+        new Actions(driver)
+                .moveToElement(txtBox)
+                .click()
+                .keyDown(Keys.SHIFT)
+                .sendKeys("a")
+                .keyUp(Keys.SHIFT)
+                .sendKeys("hmet")
+                .build()
+                .perform();
+
+        BekleKapat();
+    }
+}
