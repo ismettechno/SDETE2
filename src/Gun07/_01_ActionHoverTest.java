fc97745ed22c169ad99b56b6211bedad1d80feba
TechnoStudy <ismettechno@gmail.com>
2026-04-28 18:09:59 +0100

    _01_ActionHoverTest

---
+++ src/Gun07/_01_ActionHoverTest.java
@@ -0,0 +1,26 @@
+package Gun07;
+
+import Utility.BaseDriver;
+import Utility.MyFunc;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+public class _01_ActionHoverTest extends BaseDriver {
+
+    @Test
+    public void test1() {
+        driver.get("https://www.hepsiburada.com/");
+        MyFunc.Bekle(2);
+
+        WebElement element=driver.findElement(By.xpath("//*[text()='Moda']"));
+
+        new Actions(driver).moveToElement(element).build().perform();
+
+        BekleKapat();
+    }
+
+
+
+}
