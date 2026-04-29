0a72b4c32387e9a1fcbf263f0cccc86cf0e1aa23
TechnoStudy <ismettechno@gmail.com>
2026-04-29 19:35:22 +0100

    _05_Alertler 1

---
+++ src/Gun08/_05_Alertler.java
@@ -0,0 +1,28 @@
+package Gun08;
+
+import Utility.BaseDriver;
+import Utility.MyFunc;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class _05_Alertler extends BaseDriver {
+
+    @Test
+    public void Test1() {
+        driver.get("https://www.selenium.dev/selenium/web/alerts.html");
+        MyFunc.Bekle(2);
+
+        WebElement clickMe= driver.findElement(By.id("alert"));
+        clickMe.click();
+        MyFunc.Bekle(2);
+        //su anda alert çıkmış oldu
+
+        driver.switchTo().alert().accept();  // Alertin OK butonuna bas
+
+        BekleKapat();
+    }
+
+
+
+}
