afc90d01c0bb39ad6123da0757997c33cf329238
TechnoStudy <ismettechno@gmail.com>
2026-04-29 18:42:15 +0100

    _02_SelectClass

---
+++ src/Gun08/_02_SelectClass.java
@@ -0,0 +1,38 @@
+package Gun08;
+
+import Utility.BaseDriver;
+import Utility.MyFunc;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class _02_SelectClass extends BaseDriver {
+
+    @Test
+    public void test1() {
+        driver.get("https://www.amazon.com.tr/");
+        MyFunc.Bekle(2);
+
+        WebElement webSelectAnaElement=driver.findElement(By.id("searchDropdownBox"));
+        WebElement optionKitaplar=driver.findElement(By.xpath("//option[text()='Kitaplar']"));
+
+        //1.Yöntem
+//        webSelectAnaElement.click();
+//        optionKitaplar.click();
+
+        //2.Yöntem
+//        optionKitaplar.click();
+
+        //Eğer bu elemntin TAG SELECT ise
+        Select menuSelect=new Select(webSelectAnaElement);
+        menuSelect.selectByVisibleText("Ev");
+//        menuSelect.selectByValue("search-alias=stripbooks");
+//        menuSelect.selectByIndex(11);
+
+        for(WebElement e: menuSelect.getOptions())
+            System.out.println(e.getText());
+
+        BekleKapat();
+    }
+}
