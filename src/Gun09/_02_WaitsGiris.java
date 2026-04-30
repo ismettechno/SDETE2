843a0b9cddcc0e58cb9aed5b949dc5f7ccbe98a2
TechnoStudy <ismettechno@gmail.com>
2026-04-30 18:20:33 +0100

    _02_WaitsGiris

--- src/Gun09/_02_WaitsGiris.java
+++ src/Gun09/_02_WaitsGiris.java
@@ -1,10 +1,25 @@
 package Gun09;
 
 import Utility.BaseDriver;
+import Utility.MyFunc;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class _02_WaitsGiris extends BaseDriver {
 
+    @Test
+    public void Test1() {
+        driver.get("https://letcode.in/waits");
 
+        WebElement buton = driver.findElement(By.id("accept"));
+        buton.click();
 
+        MyFunc.Bekle(30); // java kodunu bekletiyor, ama web den haberi yok
+        driver.switchTo().alert().accept();
+        //bana öyle bir bekletme lazım ki web sayfasından hebri olacak ve gerekitiği kadar bekleyecek
+
+        BekleKapat();
+    }
 
 }
