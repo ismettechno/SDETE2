59c30a0b7bb522dd56db50303c2c187c16e3152d
TechnoStudy <ismettechno@gmail.com>
2026-05-06 18:30:37 +0100

    _05_RobotFileUpload 2

--- src/Gun11/_05_RobotFileUpload.java
+++ src/Gun11/_05_RobotFileUpload.java
@@ -2,7 +2,10 @@ package Gun11;
 
 import Utility.BaseDriver;
 import Utility.MyFunc;
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
@@ -27,8 +30,39 @@ public class _05_RobotFileUpload extends BaseDriver {
         rbt.keyRelease(KeyEvent.VK_ENTER);
         MyFunc.Bekle(2);
 
+        //10 kere TAB a bas bırak : Açılan pencereye gelecek
+        for (int i = 0; i < 9; i++) {
+            rbt.keyPress(KeyEvent.VK_TAB);
+            rbt.keyRelease(KeyEvent.VK_TAB);
+        }
 
+        //10 kere TAB a bas bırak : Açılan pencereye gelecek
+        rbt.keyPress(KeyEvent.VK_DOWN);
+        rbt.keyRelease(KeyEvent.VK_DOWN);
 
+        // sonra ENTER : desktop gözükmesi için
+        rbt.keyPress(KeyEvent.VK_ENTER);
+        rbt.keyRelease(KeyEvent.VK_ENTER);
+        MyFunc.Bekle(1);
+
+        // sağ satafa geçildi
+        rbt.keyPress(KeyEvent.VK_TAB);
+        rbt.keyRelease(KeyEvent.VK_TAB);
+        MyFunc.Bekle(1);
+
+        //10 kere TAB a bas bırak : Açılan pencereye gelecek
+        for (int i = 0; i < 4; i++) {
+            rbt.keyPress(KeyEvent.VK_DOWN);
+            rbt.keyRelease(KeyEvent.VK_DOWN);
+        }
+
+        // sonra ENTER : seçilen dosya kabul edildi
+        rbt.keyPress(KeyEvent.VK_ENTER);
+        rbt.keyRelease(KeyEvent.VK_ENTER);
+
+        MyFunc.Bekle(1);
+        WebElement dosyaSecildiMesaji=driver.findElement(By.cssSelector("div[class='file']+p"));
+        Assert.assertTrue("Dosya seçilemedi", dosyaSecildiMesaji.getText().toLowerCase().contains("test.txt"));
 
         BekleKapat();
     }
